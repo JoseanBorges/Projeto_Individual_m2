13f40f637ef7838f211aa8b662f3b4515acf1ede
Josean_Borges <132008054+JoseanBorges@users.noreply.github.com>
2023-05-11 22:18:54 -0300

    Criação de listas
    Foram criadas 02 listas para armazenar nome e nota.

---
+++ Projeto_Individual_m2.py
@@ -0,0 +1,15 @@
+candidatos = ['Antônio', 'Miguel', 'Renata', 'Juliana', 'Samuel']
+notas_candidatos = ['e6_t8_p4_s7', 'e4_t5_p3_s8', 'e7_t2_p6_s4', 'e4_t5_p7_s5', 'e8_t3_p6_s6']
+print(candidatos[0], notas_candidatos[0])
+print(candidatos[1], notas_candidatos[1])
+print(candidatos[2], notas_candidatos[2])
+print(candidatos[3], notas_candidatos[3])
+print(candidatos[4], notas_candidatos[4])
+
+
+'''def selecao_candidatos(candidatos, notas):
+    entrevista = int(notas[4])
+    t_teorico = int(notas[4])
+    t_pratico = int(notas[8])
+    s_skills = int(notas[8])'''
+
