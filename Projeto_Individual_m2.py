3237a0815ef6006c810e0f2dc142c201561346e1
Josean_Borges <132008054+JoseanBorges@users.noreply.github.com>
2023-05-18 17:44:51 -0300

    Inseridas as funções
    Estrutura das funções.

--- Projeto_Individual_m2.py
+++ Projeto_Individual_m2.py
@@ -1,15 +1,38 @@
-candidatos = ['Antônio', 'Miguel', 'Renata', 'Juliana', 'Samuel']
-notas_candidatos = ['e6_t8_p4_s7', 'e4_t5_p3_s8', 'e7_t2_p6_s4', 'e4_t5_p7_s5', 'e8_t3_p6_s6']
-print(candidatos[0], notas_candidatos[0])
-print(candidatos[1], notas_candidatos[1])
-print(candidatos[2], notas_candidatos[2])
-print(candidatos[3], notas_candidatos[3])
-print(candidatos[4], notas_candidatos[4])
+#Função para fazer a busca dos candidatos.
+def busca_candidato():
 
+candidatos = []
+#Laço para adicionar/cadastrar candidato e adicionar notas.
+while True:
+    nome_candidato = input('Digite o nome do candidato: ')
+    ent = int(input('Resultado da entrevista: '))
+    tt = int(input('Resultado do Teste Teórico: '))
+    tp = int(input('Resultado do Teste Prático: '))
+    ss = int(input('Resultado das Soft Skills: '))
 
-'''def selecao_candidatos(candidatos, notas):
-    entrevista = int(notas[4])
-    t_teorico = int(notas[4])
-    t_pratico = int(notas[8])
-    s_skills = int(notas[8])'''
+    candidatos.append([nome_candidato, ent, tt, tp, ss])
+    #Outro laço para adicionar mais candidatos.
+    while True:
+        inserir_candidato = input('\nDeseja inserir outro candidato? (Digite "s/S" para "sim" ou "n/N" para sair): ')
 
+        if inserir_candidato in 'Nn':
+            break
+        elif inserir_candidato in 'Ss':
+            break
+        else:
+            print('Opção inválida, tente novamente.')
+    
+    if inserir_candidato in 'Nn':
+        break
+
+#Imprimindo os candidatos inseridos.
+print('Candidatos inseridos')
+for cand in candidatos:
+    formato_cand = '{} - e{}_t{}_p{}_s{}'.format(cand[0], cand[1], cand[2], cand[3], cand[4])
+    print(formato_cand)
+
+#Coletando as notas mínimas.
+print('\nDigite as notas mínimas: ')
+
+#Mostrando os candidatos que atendem às notas.
+print('\nCandidatos encontrados: ')
